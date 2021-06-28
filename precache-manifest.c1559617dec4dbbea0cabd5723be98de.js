7c06b71793385b967c148b644a917fd9de7c701c
hhn20 <harshahnarayan@gmail.com>
2021-06-28 12:48:21 +0530

    Updates

---
+++ precache-manifest.c1559617dec4dbbea0cabd5723be98de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146a9871eb813979bdae2207cb1eb2b7",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e42e9de143c822c5a2e8",
+    "url": "/todo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1e6dc70cd6639685efd3",
+    "url": "/todo/static/css/main.2d474487.chunk.css"
+  },
+  {
+    "revision": "e42e9de143c822c5a2e8",
+    "url": "/todo/static/js/2.6c742efc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.6c742efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6dc70cd6639685efd3",
+    "url": "/todo/static/js/main.dcf7e150.chunk.js"
+  },
+  {
+    "revision": "036674456fec622e875f",
+    "url": "/todo/static/js/runtime-main.f3e7dc00.js"
+  }
+]);
